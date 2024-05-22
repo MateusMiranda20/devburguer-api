c56d660740eb0cea63c63741b37f5ba8d84f6396
MateusMiranda20 <mateuusmiranda20@gmail.com>
2024-05-22 20:10:47 -0300

    SessionController

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,10 @@
 import { Router } from 'express';
 import UserContoller from './app/controllrs/UserController'
+import SessionController from './app/controllrs/SessionController';
 
 const routes = new Router()
 
 routes.post('/users', UserContoller.store);
+routes.post('/session', SessionController.store);
 
 export default routes
