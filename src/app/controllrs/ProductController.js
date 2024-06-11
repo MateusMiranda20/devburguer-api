869fdd524c7b62f2ff711a774448baf08c15a710
MateusMiranda20 <mateuusmiranda20@gmail.com>
2024-06-11 01:40:29 -0300

    Finalizamos a parte multer

--- src/app/controllrs/ProductController.js
+++ src/app/controllrs/ProductController.js
@@ -1,7 +1,8 @@
 import * as Yup from 'yup'
+import Product from '../models/Products';
 
 class ProductController {
-    async store(request, response){
+    async store(request, response) {
         const schema = Yup.object({
             name: Yup.string().required(),
             price: Yup.number().required(),
@@ -9,12 +10,28 @@ class ProductController {
         });
 
         try {
-            schema.validateSync(request.body, {abortEarly: false})
+            schema.validateSync(request.body, { abortEarly: false })
         } catch (err) {
-            return response.status(400).json({error: err.errors})
+            return response.status(400).json({ error: err.errors })
         }
 
-        return response.status(201).json({message: 'Ok'})
+        const { filename: path } = request.file;
+        const { name, price, category } = request.body
+
+        const product = await Product.create({
+            name,
+            price,
+            category,
+            path
+        })
+
+        return response.status(201).json({ product})
+    }
+
+    async index(request, response) {
+        const products = await Product.findAll();
+
+        return response.json(products)
     }
 }
 
