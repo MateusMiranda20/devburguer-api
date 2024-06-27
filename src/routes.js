a153c41dc0e4e275545fbcff4084604478717766
MateusMiranda20 <mateuusmiranda20@gmail.com>
2024-06-26 23:27:11 -0300

    Craindo authorization

--- src/routes.js
+++ src/routes.js
@@ -4,6 +4,7 @@ import multerConfig from './config/multer'
 import UserContoller from './app/controllrs/UserController'
 import SessionController from './app/controllrs/SessionController';
 import ProductController from './app/controllrs/ProductController';
+import authMiddleware from './middlewares/auth';
 
 const routes = new Router()
 
@@ -11,6 +12,9 @@ const upload = multer(multerConfig)
 
 routes.post('/users', UserContoller.store);
 routes.post('/session', SessionController.store);
+
+routes.use(authMiddleware)
+
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index)
 
