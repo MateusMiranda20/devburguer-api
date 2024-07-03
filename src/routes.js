038ec89948c8794bf01680fbf318d920a6272947
MateusMiranda20 <mateuusmiranda20@gmail.com>
2024-07-03 05:35:49 -0300

    Craindo a categoria

--- src/routes.js
+++ src/routes.js
@@ -5,6 +5,7 @@ import UserContoller from './app/controllrs/UserController'
 import SessionController from './app/controllrs/SessionController';
 import ProductController from './app/controllrs/ProductController';
 import authMiddleware from './middlewares/auth';
+import CategoryController from './app/controllrs/CategoryController';
 
 const routes = new Router()
 
@@ -17,5 +18,8 @@ routes.use(authMiddleware)
 
 routes.post('/products', upload.single('file'), ProductController.store);
 routes.get('/products', ProductController.index)
+
+routes.post('/categories', CategoryController.store);
+routes.get('/categories', CategoryController.index)
 
 export default routes
