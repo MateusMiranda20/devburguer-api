4800cbb7d06b44659ead8b93c0be79ca9782bc6a
MateusMiranda20 <mateuusmiranda20@gmail.com>
2024-08-23 11:52:32 -0300

    Finalizando o projeto

--- src/app/controllers/CategoryController.js
+++ src/app/controllers/CategoryController.js
@@ -6,6 +6,7 @@ class CategoryController {
     async store(request, response) {
         const schema = Yup.object({
             name: Yup.string().required(),
+
         });
 
         try {
@@ -16,9 +17,9 @@ class CategoryController {
 
         const { name } = request.body
 
-        const { admin: isAdmin} =  await User.findByPk(request.UserId)
+        const { admin: isAdmin } = await User.findByPk(request.UserId)
 
-        if(!isAdmin){
+        if (!isAdmin) {
             return response.status(401).json()
         }
 
@@ -32,8 +33,11 @@ class CategoryController {
             return response.status(400).json({ error: 'Category already exists.' })
         }
 
+        const { filename: path } = request.file
+
         const { id } = await Category.create({
-            name
+            name,
+            path
         })
 
         return response.status(201).json({ id, name })
@@ -43,6 +47,63 @@ class CategoryController {
         const categories = await Category.findAll();
 
         return response.json(categories)
+    }
+
+    async update(request, response) {
+        const schema = Yup.object({
+            name: Yup.string(),
+
+        });
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
+        const { admin: isAdmin } = await User.findByPk(request.UserId)
+
+        if (!isAdmin) {
+            return response.status(401).json()
+        }
+
+        const { name } = request.body
+
+        if (name) {
+            const categoryExists = await Category.findOne({
+                where: {
+                    name,
+                },
+            });
+    
+            if (categoryExists && categoryExist.id !== +id) {
+                return response.status(400).json({ error: 'Category already exists.' })
+            }
+        }
+
+        const {id} = request.params
+
+        let path;
+        if(request.file){
+            path = request.file.filename
+        }
+
+        const categoryExist = await Category.findByPk(id);
+
+        if(!categoryExist){
+            return response.status(400).json({error:"Make sure your ID is Correct"})
+        }
+
+        await Category.update({
+            name,
+            path,
+        },{
+            where: {
+                id,
+            },
+        })
+
+        return response.status(200).json()
     }
 }
 
--- src/app/controllers/ProductController.js
+++ src/app/controllers/ProductController.js
@@ -8,7 +8,8 @@ class ProductController {
         const schema = Yup.object({
             name: Yup.string().required(),
             price: Yup.number().required(),
-            category_id: Yup.number().required()
+            category_id: Yup.number().required(),
+            offer:Yup.boolean()
         });
 
         try {
@@ -24,13 +25,14 @@ class ProductController {
         }
 
         const { filename: path } = request.file;
-        const { name, price, category_id } = request.body
+        const { name, price, category_id, offer } = request.body
 
         const product = await Product.create({
             name,
             price,
             category_id,
-            path
+            path,
+            offer
         })
 
         return response.status(201).json( product)
@@ -49,6 +51,57 @@ class ProductController {
 
         return response.json(products)
     }
+
+    async update(request, response){
+        const schema = Yup.object({
+            name: Yup.string(),
+            price: Yup.number(),
+            category_id: Yup.number(),
+            offer:Yup.boolean()
+        });
+
+        try {
+            schema.validateSync(request.body, { abortEarly: false })
+        } catch (err) {
+            return response.status(400).json({ error: err.errors })
+        }
+
+        const { admin: isAdmin} =  await User.findByPk(request.UserId)
+
+        if(!isAdmin){
+            return response.status(401).json()
+        }
+
+        const { id } = request.params;
+
+        const findProduct = await Product.findByPk(id);
+
+        if(!findProduct){
+            return response.status(400).json({error:"Make sure your ID is Correct"})
+        }
+
+        let path;
+        if(request.file){
+            path = request.file.filename
+        }
+
+        const { name, price, category_id, offer } = request.body
+
+        const product = await Product.update({
+            name,
+            price,
+            category_id,
+            path,
+            offer
+        }, {
+            where:{
+                id,
+            }
+        })
+
+        return response.status(201).json( product)
+    }   
+    
 }
 
 export default new ProductController();
