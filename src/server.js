3aed15f564cfc574dea3ec8bfd2e3c3477f8a263
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-03-05 13:38:38 -0300

    PORT

--- src/server.js
+++ src/server.js
@@ -2,5 +2,5 @@ import app from './app'
 import 'dotenv/config';
 
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 4000
 app.listen(PORT, () => console.log('Server is running ar port 4000 ..'))
