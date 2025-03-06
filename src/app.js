e8eb342d7cfe2eb3982d6a042fada44fb38ae50f
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-03-06 13:59:23 -0300

    Cors

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ import './database';
 class App {
   constructor() {
     this.app = express()
-    this.app.use(cors())
+    this.app.use(cors({
+      origin: "https://hamburgueria-front-n5tv.vercel.app/", // Substitua pela URL do frontend
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true, // Permite cookies e headers de autenticação
+    }));
+    
 
     this.middlewares()
 
