85d0f20cac48312a3aa33e95789e2225b220678f
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-03-08 21:15:52 -0300

    config/database

--- src/config/database.js
+++ src/config/database.js
@@ -1,25 +1,29 @@
 import 'dotenv/config';
 
 module.exports = {
+  development: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true, // Exige SSL
-        rejectUnauthorized: false // Permite certificados autoassinados
-      }
-    },
     define: {
       timestamps: true,
       underscored: true,
       underscoredAll: true,
     }
-}
-
+  },
+  production: {
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    define: {
+      timestamps: true,
+      underscored: true,
+      underscoredAll: true,
+    }
+  }
+};
 
 
 // Definir a URL base do backend
