a51ebcf52d3bedca027c705d49429d72ea3b0639
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-03-09 20:41:02 -0300

    Configurando o SSL=true

--- src/config/database.js
+++ src/config/database.js
@@ -1,38 +1,33 @@
 require('dotenv/config');
 
 module.exports = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  ssl: process.env.DATABASE_SSL === `true`,
+  dialect: process.env.DB_DIALECT || 'postgres',
+  dialectOptions: process.env.DB_SSL === "true" ? { ssl: { require: true, rejectUnauthorized: false } } : {},
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    ssl: process.env.DATABASE_SSL === `true`,
-    dialect: process.env.DB_DIALECT || 'postgres',
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false, // Isso evita problemas com certificados SSL
-      },
-    },
+    dialect: "postgres",
     define: {
       timestamps: true,
       underscored: true,
       underscoredAll: true,
-    },
-    production: {
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT,
-      dialect: "postgres",
-      define: {
-        timestamps: true,
-        underscored: true,
-        underscoredAll: true,
-      }
+    }
   }
-  
+
 };
 
 
