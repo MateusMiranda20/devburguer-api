22f98e5474db62422d80ff242ef57078eedab2e3
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-03-14 20:41:31 -0300

    Tets SSL=false

--- src/config/database.js
+++ src/config/database.js
@@ -6,9 +6,9 @@ module.exports = {
   database: process.env.DB_DATABASE,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
-  ssl: process.env.DATABASE_SSL === `true`,
+  ssl: process.env.DATABASE_SSL === `false`,
   dialect: process.env.DB_DIALECT || 'postgres',
-  dialectOptions: process.env.DB_SSL === "true" ? { ssl: { require: true, rejectUnauthorized: false } } : {},
+  dialectOptions: process.env.DB_SSL === "false" ? { ssl: { require: false, rejectUnauthorized: false } } : {},
   define: {
     timestamps: true,
     underscored: true,
