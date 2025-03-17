8ce0880e24fd20a35aa764cec124a3460580703f
MateusMiranda20 <mateuusmiranda20@gmail.com>
2025-03-17 14:14:23 -0300

    key secrect new

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,4 +1,4 @@
 export default {
-    secret : '975359016138b0223ab789505f7dbeca',
-    expiresIn: '10d'
+    secret: process.env.JWT_SECRET || '975359016138b0223ab789505f7dbeca',
+    expiresIn: '5d'
 };
